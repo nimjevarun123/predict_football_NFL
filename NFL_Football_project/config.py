2ee191382c0d498dd447c2943f2fcc5f94f63ff0
nimjevarun123 <128397824+nimjevarun123@users.noreply.github.com>
2023-03-30 02:49:32 +0530

    Update config.py

--- NFL_Football_project/config.py
+++ NFL_Football_project/config.py
@@ -5,6 +5,6 @@ import pickle
 model_path = r"artifacts\binary_clas_model.pkl"
 json_path = r'artifacts\Input_Features.json'
 
-HOST = "127.0.0.1"
-PORT = 5000
-DEBUG = True
+HOST = "0.0.0.0"
+PORT = 8000
+DEBUG = False
